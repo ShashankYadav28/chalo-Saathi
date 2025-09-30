6a780706f8d9b311962e915c7ba396f2499e4cdf
Shashank Yadav <ShashankYadav2803@gmail.com>
2025-09-30 16:14:00 +0530

    Initial Commit

---
+++ ChaloSaathi/ChaloSaathiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaloSaathiApp.swift
+//  ChaloSaathi
+//
+//  Created by Shashank Yadav on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ChaloSaathiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChaloSaathiTests/ChaloSaathiTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChaloSaathiTests.swift
+//  ChaloSaathiTests
+//
+//  Created by Shashank Yadav on 30/09/25.
+//
+
+import Testing
+@testable import ChaloSaathi
+
+struct ChaloSaathiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
