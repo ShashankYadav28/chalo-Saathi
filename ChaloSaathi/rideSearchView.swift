93a66a904b6c09839b20322eeff61d514b175291
Shashank Yadav <ShashankYadav2803@gmail.com>
2025-10-01 10:33:07 +0530

    feat: add splash screen

---
+++ ChaloSaathi/rideSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  rideSearchView.swift
+//  ChaloSaathi
+//
+//  Created by Shashank Yadav on 01/10/25.
+//
+
+import SwiftUI
+
+struct rideSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    rideSearchView()
+}
