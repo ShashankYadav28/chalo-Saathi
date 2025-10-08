c46285f2eb1e2551461bfba590bad68932f0c68d
Shashank Yadav <ShashankYadav2803@gmail.com>
2025-10-08 17:06:54 +0530

    feat : ride and UserModle setup

---
+++ ChaloSaathi/Models/Ride.swift
@@ -0,0 +1,18 @@
+//
+//  RiderModel.swift
+//  ChaloSaathi
+//
+//  Created by Shashank Yadav on 08/10/25.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct Ride:  {
+    
+    
+  
+    
+}
+
+
