b580fd9e43233b2841454ad3f1b8bb50d849dcc7
Shashank Yadav <ShashankYadav2803@gmail.com>
2025-10-08 17:07:56 +0530

    user profile and Ride Setup

--- ChaloSaathi/Models/Ride.swift
+++ ChaloSaathi/Models/Ride.swift
@@ -7,8 +7,74 @@
 
 import Foundation
 import FirebaseFirestore
+import FirebaseFirestoreCombineSwift
 
-struct Ride:  {
+struct Ride: Identifiable, Codable {
+    
+    @DocumentID var id:String?
+    let userId:String
+    let userName: String
+    let userGender: String
+    let userProfile: String?
+    
+    
+    let fromAddress: String
+    let fromLat: Double
+    let fromLong: Double
+    
+    let toAddress: String
+    let toLat: String
+    let toLong: String
+    
+    let date:Date
+    let time: String
+    let availableSeats:Int
+    let vehicleType:String
+    
+    
+    let farePerKm: Double?
+    let TotalFare: Double?
+    let estimatedDistance: Double?
+    
+    
+    let allowGender:String
+    
+    let status: String
+    let createdAt: Date
+    
+    
+    enum CodingKeys:  String,CodingKey  {
+        case id
+        case userId
+        case userName
+        case userGender
+        case userProfile
+        case fromAddress
+        case fromLat
+        case fromLong
+        
+        case toAddress
+        case toLat
+        case toLong
+        
+        case date
+        case time
+        case availableSeats
+        case vehicleType
+        
+        case farePerKm
+        case TotalFare
+        case estimatedDistance
+        
+        case allowGender
+        
+        case status
+        case createdAt
+        
+       
+        
+        
+    }
     
     
   
--- ChaloSaathi/Models/UserModel.swift
+++ ChaloSaathi/Models/UserModel.swift
@@ -5,12 +5,36 @@
 //  Created by Shashank Yadav on 03/10/25.
 //
 
-struct UserProfile : Codable  {
-    var id: String
-    var name: String
-    var email: String
-    var gender: String
-    var aadhaar: String
-    var vehicleType: String 
+import Foundation
+import FirebaseFirestore
+
+struct UserProfile: Identifiable,Codable  {
+    @DocumentID var id:String?
+    let  userId: String
+    let  name: String
+    let  email: String
+    let  gender: String
+    let  aadhaar: String?
+    let  vehicleType: String?
+    let  profilePicture: String?
+    let  fcmToken: String?
+    let  createdAt: Date
+    
+    enum CodingKeys: String , CodingKey {
+        case id
+        case userId
+        case name
+        case email
+        case gender
+        case aadhaar
+        case vehicleType
+        case profilePicture
+        case fcmToken
+        case createdAt
+        
+        
+        
+    }
+    
     
 }
