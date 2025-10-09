8085d896b7a437ccb3f1d02ab1a6ff204f3032ee
Shashank Yadav <ShashankYadav2803@gmail.com>
2025-10-09 18:39:37 +0530

    Location ride search setup

---
+++ ChaloSaathi/Location/LocationManagerRideSearch.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManagerRideSearch.swift
+//  ChaloSaathi
+//
+//  Created by Shashank Yadav on 09/10/25.
+//
+
+import Foundation
---
+++ ChaloSaathi/Models/AppuserProfile .swift
@@ -0,0 +1,18 @@
+//
+//  AppuserProfile .swift
+//  ChaloSaathi
+//
+//  Created by Shashank Yadav on 08/10/25.
+//
+
+import SwiftUI
+
+struct AppuserProfile_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppuserProfile_()
+}
